ae889f4df23ed3dc179a7c34a8e8a8a23d1ca19c
Doni <tkyskii2004@gmail.com>
2023-11-21 20:57:29 +0600

    "Add Api"

--- routes/routes.js
+++ routes/routes.js
@@ -1,109 +1,139 @@
 const express = require('express');
 const router = express.Router();
 const Student = require('../models/students');
-const multer = require('multer');
-const students = require('../models/students');
 
+const professions = ['Engineer', 'Doctor', 'IT', 'Other'];
+const courses = ['1','2','3','4','5','6','7','8','9', 'Other'];
 
-var storage = multer.diskStorage({
-    destination:function(req, file,cb){
-        cb(null, './uploads')
-    },
-    filename:function(req, file,cb){
-        cb(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
-    }
-});
-
-var upload = multer({
-    storage:storage,
-}).single("image");
-
-router.post('/addStud',upload,(req,res) => {
-    const student = new Student({
-        name:req.body.name,
-        email:req.body.email,
-        profession:req.body.profession,
-        course:req.body.course,
+router.route('/addStud')
+    .get((req, res) => {
+        const selectedCourse = 'defaultValue';
+        const selectedProfession='defaultValue'
+        res.render("add_students", { professions, courses, selectedCourse,selectedProfession });
     })
-    student.save().
-    then(() => {
-        console.log("Student Added successfully")
-    }).catch((err) => {
-        console.log("Student added unsuccessfully")
-    })
-    res.redirect('/')
-});
+    .post((req, res) => {
+        const { name, email, profession, course } = req.body;
+        if (!professions.includes(profession) || !courses.includes(course)) {
+            return res.status(400).send('Invalid profession or course');
+        }
+        res.redirect('/');
+    });
 
+router.route('/api/addStud')
+    .post((req, res) => {
+        const student = new Student({
+            name: req.body.name,
+            email: req.body.email,
+            profession: req.body.profession,
+            course: req.body.course,
+        });
 
-
-router.get('/',(req,res) =>{
-    Student.find().exec((err,students) => {
-        if(err){
-            res.json({message:err.message});
-        }else{
-            res.render('index',{
-                title:'Home Page',
-                students:students,
+        student.save()
+            .then(() => {
+                res.render('/')
             })
-        }
-    })
-});
-router.get('/addStud',(req,res) => {
-    res.render("add_students", { title: "Add Students" });
-});
-router.get('/edit/:id', (req,res) => {
-     let id = req.params.id;
-     students.findById(id, (err, student) => {
-        if(err){
-            res.redirect('/');
-        }else{
-            if(student == null){
-                res.redirect('/');
-            }else{
-                res.render("edit_students",{
-                    title:"Edit Students",
-                    student:student,
-                })
+            .catch((err) => {
+                console.error(err);
+                res.status(500).json({ error: "Internal Server Error" });
+            });
+    });
+
+router.route('/')
+    .get((req, res) => {
+        Student.find().exec((err, students) => {
+            if (err) {
+                res.json({ message: err.message });
+            } else {
+                res.render('index', {
+                    title: 'Home Page',
+                    students: students,
+                });
             }
-        }
-     })
-});
+        });
+    });
 
+router.route('/edit/:id')
+    .get((req, res) => {
+        let id = req.params.id;
+        Student.findById(id, (err, student) => {
+            if (err) {
+                res.redirect('/');
+            } else {
+                if (student == null) {
+                    res.redirect('/');
+                } else {
+                    res.render("edit_students", {
+                        title: "Edit Students",
+                        student: student,
+                        professions: professions,
+                        courses: courses,
+                    });
+                }
+            }
+        });
+    })
+    .post((req, res) => {
+        const id = req.params.id;
+        const updatedData = {
+            name: req.body.name,
+            email: req.body.email,
+            profession: req.body.profession,
+            course: req.body.course
+        };
 
-router.post('/update/:id', (req, res) => {
+        Student.findOneAndUpdate(
+            { _id: id },
+            updatedData,
+            { new: true },
+            (err, updatedStudent) => {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).json({ error: 'Could not update student' });
+                }
+
+                if (!updatedStudent) {
+                    return res.status(404).json({ error: 'Student not found' });
+                }
+                res.redirect("/");
+            }
+        );
+    });
+
+router.put('/api/edit/:id', (req, res) => {
     const id = req.params.id;
     const updatedData = {
-      name: req.body.name,
-      email: req.body.email,
-      profession: req.body.profession,
-      course: req.body.course
+        name: req.body.name,
+        email: req.body.email,
+        profession: req.body.profession,
+        course: req.body.course
     };
-  
+
     Student.findOneAndUpdate(
-      { _id: id },
-      updatedData,
-      { new: true },
-      (err, updatedStudent) => {
-        if (err) {
-          console.error(err);
-          return res.status(500).json({ error: 'Could not update student' });
+        { _id: id },
+        updatedData,
+        { new: true },
+        (err, updatedStudent) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ error: 'Could not update student' });
+            }
+
+            if (!updatedStudent) {
+                return res.status(404).json({ error: 'Student not found' });
+            }
+
+            res.json({ message: 'Student updated successfully', student: updatedStudent });
         }
-  
-        if (!updatedStudent) {
-          return res.status(404).json({ error: 'Student not found' });
-        }
-        res.redirect("/");
-      }
     );
-  });
-  
+});
 
-
-router.get('/delete/:id', (req,res) => {
-    let id = req.params.id;
-    Student.findByIdAndDelete(id, (res) => {
-    })  
-    res.redirect("/");
-})
+router.route('/delete/:id')
+    .get((req, res) => {
+        let id = req.params.id;
+        Student.findByIdAndDelete(id, (res) => {
+        });
+        res.redirect("/");
+    });
 
 module.exports = router;
+
